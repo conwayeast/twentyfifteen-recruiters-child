da0d3ef26d1dec415c89e2268567d37e4b7ccfc1
Ryan Conway <conwayeast@gmail.com>
2015-04-24 19:54:48 -0400

    Adding bourbon and neat and fixing resume page.

--- content-resume.php
+++ content-resume.php
@@ -37,53 +37,58 @@
 
 		?>
 
-			<!-- WORK EXPERIENCE -->
-		<header>
-			<h2>Work Experience</h2>
-		</header><!-- .entry-header -->
+	<!-- WORK EXPERIENCE -->
+	<header>
+		<h2 style="margin-top: 0; margin-bottom: 0;">Work Experience</h2>
+	</header><!-- /header -->
+
+<div style="margin-bottom: 3em;">
+
+
 
 		<?php $experience = new WP_Query(array(
 			'post_type'	=>	'experience'
 		)); ?>
 		<?php while($experience->have_posts()) : $experience->the_post(); ?>
-			<div>
-				<div style="width: 70%; float:left;">
-					<h3 style="margin: 0"><?php the_field( 'title' ); ?></h3>
-					<h4 style="margin: 0"><?php the_field( 'company' ); ?></h4>
+			<div class="container__what-where-when">
+				<div class="container__what-where">
+					<h3 class="title__what"><?php the_field( 'title' ); ?></h3>
+					<h4 class="title__where"><?php the_field( 'company' ); ?></h4>
 				</div>
-				<div style="width: 30%; float:left; background: red; color: white; padding: 0.5em 0.25em;">
-					<h4 style="margin: 0; font-size: 0.95em; text-align: center;"><?php the_field( 'dates' ); ?></h4>
+				<div class="container__when">
+					<h4 class="title__when"><?php the_field( 'dates' ); ?></h4>
 				</div>
-				<br><br>
-				<p><?php the_field( 'detailed_description' ); ?></p>
-				<hr>
+				<div class="container__description">
+					<?php the_field( 'detailed_description' ); ?>
+				</div>
 			</div>
 		<?php endwhile; ?>
+</div>
 
 
+<div style="margin-top: 3em;">
 	<!-- EDUCATION -->
 		<header>
 			<h2>Education</h2>
-		</header><!-- .entry-header -->
+		</header><!-- /header -->
 
 		<?php $school = new WP_Query(array(
 				'post_type'	=>	'school'
 			)); ?>
 			<?php while($school->have_posts()) : $school->the_post(); ?>
-			<div>
-				<div style="width: 70%; float:left;">
-					<h3 style="margin: 0"><?php the_field( 'school' ); ?></h3>
-					<h4 style="margin: 0"><?php the_field( 'degree' ); ?></h4>
+			<div class="container__what-where-when">
+				<div class="container__what-where">
+					<h3 class="title__what"><?php the_field( 'school' ); ?></h3>
+					<h4 class="title__where"><?php the_field( 'degree' ); ?></h4>
 				</div>
-				<div style="width: 30%; float:left; background: red; color: white; padding: 0.5em 0.25em;">
-					<h4 style="margin: 0; font-size: 0.95em; text-align: center;"><?php the_field( 'dates' ); ?></h4>
+				<div class="container__when">
+					<h4 class="title__when"><?php the_field( 'dates' ); ?></h4>
 				</div>
 			</div>
-			<br><br>
-			<p style="margin-top: 1em;"><?php the_field( 'detailed_description' ); ?></p>
-			<hr>
-		<?php endwhile; ?>
-			</div><!-- .entry-content -->
+			<?php endwhile; ?>
+</div>
+
+	</div><!-- .entry-content -->
 
 
 	<footer class="entry-footer">
